c4ea13de8e603f6b3908995731097fab7fc79faf
Riya-Pallikala <rpallika@purdue.edu>
2023-10-31 17:08:03 -0400

    - Reset database complete (commented out in "if main") - adding books works -- update to only allow authors to add - now edit to only return matches in recommendation form

--- app.py
+++ app.py
@@ -10,11 +10,14 @@ def home():
     success = False
     return render_template('main_page.html', success=success)
 
-@app.route('/', methods=['POST', 'GET'])
+# @app.route('/', methods=['POST', 'GET'])
 def reset_database():
     # Change to stored procedure?
     conn = sqlite3.connect('databases/test_db.db')
     cursor = conn.cursor()
+
+    cursor.execute('DROP TABLE IF EXISTS Books;')
+    cursor.execute('DROP TABLE IF EXISTS Authors;')
 
     # Create Authors(authorId, firstname, lastname, birth_year, death_year)
     cursor.execute('CREATE TABLE IF NOT EXISTS Authors '
@@ -22,7 +25,16 @@ def reset_database():
                    'firstname TEXT, '
                    'lastname TEXT);')
 
-    cursor.execute('INSERT INTO Authors (authorId, firstname, lastname) VALUES (?, ?, ?);', (1, "fname", "lname"))
+    cursor.execute('INSERT INTO Authors (authorId, firstname, lastname) VALUES (?, ?, ?);', (1, "Leigh", "Bardugo"))
+    cursor.execute('INSERT INTO Authors (authorId, firstname, lastname) VALUES (?, ?, ?);', (2, "Neil", "Gaiman"))
+    cursor.execute('INSERT INTO Authors (authorId, firstname, lastname) VALUES (?, ?, ?);', (3, "William", "Shakespeare"))
+    cursor.execute('INSERT INTO Authors (authorId, firstname, lastname) VALUES (?, ?, ?);', (4, "Agatha", "Christie"))
+    cursor.execute('INSERT INTO Authors (authorId, firstname, lastname) VALUES (?, ?, ?);', (5, "Madeline", "Miller"))
+    cursor.execute('INSERT INTO Authors (authorId, firstname, lastname) VALUES (?, ?, ?);', (6, "Robert", "Frost"))
+    cursor.execute('INSERT INTO Authors (authorId, firstname, lastname) VALUES (?, ?, ?);', (7, "Stephen", "King"))
+    cursor.execute('INSERT INTO Authors (authorId, firstname, lastname) VALUES (?, ?, ?);', (8, "Michelle", "Obama"))
+    cursor.execute('INSERT INTO Authors (authorId, firstname, lastname) VALUES (?, ?, ?);', (9, "Kevin", "Kwan"))
+    cursor.execute('INSERT INTO Authors (authorId, firstname, lastname) VALUES (?, ?, ?);', (10, "Robin", "Kimmerer"))
 
     # Create Books(bookId, name, authorId, genre, length_in_pages)
     cursor.execute('CREATE TABLE IF NOT EXISTS Books '
@@ -32,11 +44,33 @@ def reset_database():
                    'genre TEXT, '
                    'FOREIGN KEY(authorId) REFERENCES Authors(authorId));')
 
+    cursor.execute('INSERT INTO Books (bookId, name, authorId, genre)  VALUES (?, ?, ?, ?);', (1, "Six of Crows", 1, "Fantasy"))
+    cursor.execute('INSERT INTO Books (bookId, name, authorId, genre)  VALUES (?, ?, ?, ?);', (2, "Coraline", 2, "Fantasy"))
+    cursor.execute('INSERT INTO Books (bookId, name, authorId, genre)  VALUES (?, ?, ?, ?);',
+                   (3, "Romeo and Juliet", 3, "Play"))
+    cursor.execute('INSERT INTO Books (bookId, name, authorId, genre)  VALUES (?, ?, ?, ?);',
+                   (4, "Murder on the Orient Express", 4, "Mystery"))
+    cursor.execute('INSERT INTO Books (bookId, name, authorId, genre)  VALUES (?, ?, ?, ?);',
+                   (5, "The Song of Achilles", 5, "Historical Fiction"))
+    cursor.execute('INSERT INTO Books (bookId, name, authorId, genre)  VALUES (?, ?, ?, ?);',
+                   (6, "The Road Not Taken", 6, "Poetry"))
+    cursor.execute('INSERT INTO Books (bookId, name, authorId, genre)  VALUES (?, ?, ?, ?);',
+                   (7, "It", 7, "Horror"))
+    cursor.execute('INSERT INTO Books (bookId, name, authorId, genre)  VALUES (?, ?, ?, ?);',
+                   (8, "Becoming", 8, "Autobiography"))
+    cursor.execute('INSERT INTO Books (bookId, name, authorId, genre)  VALUES (?, ?, ?, ?);',
+                   (9, "Crazy Rich Asians", 9, "Romance"))
+    cursor.execute('INSERT INTO Books (bookId, name, authorId, genre)  VALUES (?, ?, ?, ?);',
+                   (10, "Braiding Sweetgrass", 10, "Contemporary"))
+    conn.commit()
+    return
+
 @app.route('/', methods=['POST', 'GET'])
 def update_database():
     success = False
     bookname = request.form['newbook_input']
-    bookauthor = request.form['newauthor_input']
+    bookauthorfirst = request.form['newauthorfirst_input']
+    bookauthorsecond = request.form['newauthorsecond_input']
     bookgenre = request.form['newgenre_input']
 
 
@@ -46,11 +80,9 @@ def update_database():
 
     # Add to database using the user's input
 
-
-
     cursor.execute('SELECT max(bookId) from Books;')
 
-    # Calculate a new ID
+    # Calculate a new book ID
     result = cursor.fetchone()
     if (result[0] is None):
         max_id = 1
@@ -58,11 +90,32 @@ def update_database():
         max_id = result[0] + 1
 
     # Calculate author's id
+    ## SANITIZE INPUT TO AVOID SQL INJECTION ATTACKS
+    cursor.execute('SELECT authorId FROM Authors a WHERE a.firstname = ? AND a.lastname = ?', (bookauthorfirst, bookauthorsecond))
+    results = cursor.fetchone()
+    aId = 0
+
+    if (result[0] is None):
+        # New author, need to add to the Authors table
+
+        # Calculate a new author ID
+        cursor.execute('SELECT max(authorId) from Authors;')
+        result = cursor.fetchone()
+        if (result[0] is None):
+            aId = 1
+        else:
+            aId = result[0] + 1
+
+        # Add new author
+        cursor.execute('INSERT INTO Authors (authorId, firstname, lastname) VALUES (?, ?, ?);',
+                       (aId, bookauthorfirst, bookauthorsecond))
+    else:
+        aId = results[0]
 
     # Check for existing book
 
     # id, name, genre, author, length
-    cursor.execute('INSERT INTO Books (bookId, name, authorId, genre)  VALUES (?, ?, ?, ?);', (max_id, bookname, 1, bookgenre))
+    cursor.execute('INSERT INTO Books (bookId, name, authorId, genre)  VALUES (?, ?, ?, ?);', (max_id, bookname, aId, bookgenre))
     conn.commit()
     conn.close()
 
@@ -91,4 +144,5 @@ def query_database():
     return render_template('results.html', data=result)
 
 if __name__ == '__main__':
+    # reset_database()
     app.run(debug=True)
