948ea0237249d3faee4f5cd841a96a2d4c90cc3e
Riya-Pallikala <rpallika@purdue.edu>
2023-10-31 16:04:51 -0400

    Insertion and SELECT works, creating helper to reset/set up starting database

--- app.py
+++ app.py
@@ -7,14 +7,37 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('main_page.html')
+    success = False
+    return render_template('main_page.html', success=success)
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['POST', 'GET'])
+def reset_database():
+    # Change to stored procedure?
+    conn = sqlite3.connect('databases/test_db.db')
+    cursor = conn.cursor()
+
+    # Create Authors(authorId, firstname, lastname, birth_year, death_year)
+    cursor.execute('CREATE TABLE IF NOT EXISTS Authors '
+                   '(authorId INTEGER PRIMARY KEY, '
+                   'firstname TEXT, '
+                   'lastname TEXT);')
+
+    cursor.execute('INSERT INTO Authors (authorId, firstname, lastname) VALUES (?, ?, ?);', (1, "fname", "lname"))
+
+    # Create Books(bookId, name, authorId, genre, length_in_pages)
+    cursor.execute('CREATE TABLE IF NOT EXISTS Books '
+                   '(bookId INTEGER PRIMARY KEY, '
+                   'name TEXT, '
+                   'authorId INTEGER, '
+                   'genre TEXT, '
+                   'FOREIGN KEY(authorId) REFERENCES Authors(authorId));')
+
+@app.route('/', methods=['POST', 'GET'])
 def update_database():
+    success = False
     bookname = request.form['newbook_input']
     bookauthor = request.form['newauthor_input']
     bookgenre = request.form['newgenre_input']
-    booklen = request.form['newlen_input']
 
 
     # Connect to the database
@@ -23,17 +46,7 @@ def update_database():
 
     # Add to database using the user's input
 
-    # Create Authors(authorId, firstname, lastname, birth_year, death_year)
-    cursor.execute('CREATE TABLE IF NOT EXISTS Authors '
-                   '(authorId INTEGER PRIMARY KEY, '
-                   'firstname TEXT, '
-                   'lastname TEXT, '
-                   'birth_year DATETIME, '
-                   'death_year DATETIME);')
-    cursor.execute('INSERT INTO Authors VALUES (?, ?, ?, ?, ?);', (1, "fname", "lname", datetime.datetime.now(), datetime.datetime.now()))
 
-    # Create Books(bookId, name, authorId, genre, length_in_pages)
-    cursor.execute('CREATE TABLE IF NOT EXISTS Books (bookId INTEGER PRIMARY KEY, name TEXT, genre TEXT, authorId INTEGER, length_in_pages INTEGER, FOREIGN KEY(authorId) REFERENCES Authors(authorId));')
 
     cursor.execute('SELECT max(bookId) from Books;')
 
@@ -48,15 +61,16 @@ def update_database():
 
     # Check for existing book
 
-
-    cursor.execute('INSERT INTO Books VALUES (?, ?, ?, ?, ?);', (max_id, bookname, bookgenre, 1, booklen))
+    # id, name, genre, author, length
+    cursor.execute('INSERT INTO Books (bookId, name, authorId, genre)  VALUES (?, ?, ?, ?);', (max_id, bookname, 1, bookgenre))
+    conn.commit()
     conn.close()
 
-    return render_template('main_page.html')
+    success = True
+    return render_template('main_page.html', success=success)
 
 @app.route('/query', methods=['POST'])
 def query_database():
-
     genre_input = request.form['genre_input']
     author_input = request.form['author_input']
 
@@ -66,14 +80,13 @@ def query_database():
 
     # Execute an SQL query using the user's input
 
-
     cursor.execute('SELECT * FROM Books;')
     result = cursor.fetchall()
     conn.close()
 
     # Process the result and return it to the user
-    if (len(result) == 0):
-        return render_template('empty_results.html')
+    #if (len(result) == 0):
+    #    return render_template('empty_results.html')
 
     return render_template('results.html', data=result)
 
