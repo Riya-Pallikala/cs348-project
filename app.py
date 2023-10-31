9b9b0144da4169cda13c997e29a7dd8e56670bf9
Riya-Pallikala <rpallika@purdue.edu>
2023-10-31 19:13:00 -0400

    - filtering works kinda; stored procedures should work technically, but SQLite doesn't support OUTPUT params; editing to table returns to be compatible with SQLite capability, may revert here and switch to mysql if necessary

--- app.py
+++ app.py
@@ -63,11 +63,20 @@ def reset_database():
     cursor.execute('INSERT INTO Books (bookId, name, authorId, genre)  VALUES (?, ?, ?, ?);',
                    (10, "Braiding Sweetgrass", 10, "Contemporary"))
     conn.commit()
+
+    # Create stored procedures
+    with open('stored_procedures.sql', 'r') as sql_file:
+        cursor.executescript(sql_file.read())
+    conn.commit()
+    conn.close()
+
     return
 
 @app.route('/', methods=['POST', 'GET'])
 def update_database():
     success = False
+    alreadyRegistered = False
+
     bookname = request.form['newbook_input']
     bookauthorfirst = request.form['newauthorfirst_input']
     bookauthorsecond = request.form['newauthorsecond_input']
@@ -80,39 +89,41 @@ def update_database():
 
     # Add to database using the user's input
 
-    cursor.execute('SELECT max(bookId) from Books;')
-
-    # Calculate a new book ID
-    result = cursor.fetchone()
-    if (result[0] is None):
-        max_id = 1
-    else:
-        max_id = result[0] + 1
 
     # Calculate author's id
     ## SANITIZE INPUT TO AVOID SQL INJECTION ATTACKS
-    cursor.execute('SELECT authorId FROM Authors a WHERE a.firstname = ? AND a.lastname = ?', (bookauthorfirst, bookauthorsecond))
-    results = cursor.fetchone()
-    aId = 0
 
-    if (result[0] is None):
+    aId = 0
+    cursor.execute('CALL getAuthorIdFromName(?, ?, ?)', (bookauthorfirst, bookauthorsecond, aId))
+
+    if (aId is not None):
+        # Author already exists, check that they don't already have a book of this name
+
+        # Check for existing book in Books table
+        cursor.execute('SELECT * FROM Books b WHERE b.authorId == ? AND b.name == ?', (aId, bookname))
+        res = cursor.fetchone()
+        if (res[0] is not None):
+            # book already exists. Cannot register, so return failure message
+            alreadyRegistered = True
+            return render_template('main_page.html', alreadyRegistered=alreadyRegistered)
+
+    # If reaches here, then author either doesn't exist or doesn't have a book by the name. Check for nonexistence first
+    if (aId is None):
         # New author, need to add to the Authors table
 
         # Calculate a new author ID
-        cursor.execute('SELECT max(authorId) from Authors;')
-        result = cursor.fetchone()
-        if (result[0] is None):
-            aId = 1
-        else:
-            aId = result[0] + 1
+        cursor.execute('CALL getNewAuthorId(?)', aId)
 
-        # Add new author
+        # Add new author to Authors table
         cursor.execute('INSERT INTO Authors (authorId, firstname, lastname) VALUES (?, ?, ?);',
                        (aId, bookauthorfirst, bookauthorsecond))
-    else:
-        aId = results[0]
 
-    # Check for existing book
+
+    # Author has been added to database if not previously existing. Book can now be registered
+
+    # Calculate a new book ID
+    max_id = 0
+    cursor.execute('CALL getNewBookId(?)', max_id)
 
     # id, name, genre, author, length
     cursor.execute('INSERT INTO Books (bookId, name, authorId, genre)  VALUES (?, ?, ?, ?);', (max_id, bookname, aId, bookgenre))
@@ -126,6 +137,7 @@ def update_database():
 def query_database():
     genre_input = request.form['genre_input']
     author_input = request.form['author_input']
+    rating_input = request.form['rating_input']
 
     # Connect to the database
     conn = sqlite3.connect('databases/test_db.db')
@@ -133,7 +145,32 @@ def query_database():
 
     # Execute an SQL query using the user's input
 
-    cursor.execute('SELECT * FROM Books;')
+    query_str = 'SELECT * FROM Books b'
+    firstFilter = True
+    if (genre_input != ''):
+        firstFilter = False
+        query_str += ' WHERE b.genre == "' + genre_input + '"'
+
+    if (author_input != ''):
+        author_names = author_input.split(' ');
+        aId = 0;
+        cursor.execute('CALL getAuthorIdFromName(?, ?, ?)', (author_names[0], author_names[len(author_names) - 1], aId))
+        if firstFilter:
+            query_str += ' WHERE'
+        else:
+            query_str += ' AND'
+        query_str += ' b.authorId == ' + aId
+
+    if (rating_input != ''):
+        # stored procedure to calculate rating for each book
+        if firstFilter:
+            query_str += ' WHERE'
+        else:
+            query_str += ' AND'
+        query_str += ' b.rating == ' + rating_input
+
+    query_str += ';'
+    cursor.execute(query_str)
     result = cursor.fetchall()
     conn.close()
 
