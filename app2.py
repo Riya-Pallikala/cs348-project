7cbd3c5609c3f2928baa12023c7e813d5606fb70
Riya-Pallikala <rpallika@purdue.edu>
2023-12-03 02:00:54 -0500

    - changed INSERT logic to ORM, and changed reset_database to use ORM functions - created classes for Authors and Books tables

--- app2.py
+++ app2.py
@@ -10,31 +10,97 @@ def home():
     success = False
     return render_template('main_page.html', success=success)
 
+# Authors Table
+class AuthorClass:
+    def __init__(self, authorId, firstname, lastname):
+        self.authorId = authorId
+        self.firstname = firstname
+        self.lastname = lastname
+
+    # ORM insert
+    def save_to_db(self):
+        conn = sqlite3.connect('databases/test_db1.db')
+        cursor = conn.cursor()
+
+        # Insert row with passed values
+        cursor.execute('INSERT INTO Authors (authorId, firstname, lastname) VALUES (?, ?, ?)',
+                       (self.authorId, self.firstname, self.lastname))
+
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+
+# Books Table
+class BookClass:
+    def __init__(self, bookId, name, authorId, genre):
+        self.bookId = bookId
+        self.name = name
+        self.authorId = authorId
+        self.genre = genre
+
+    # ORM insert
+    def save_to_db(self):
+        conn = sqlite3.connect('databases/test_db1.db')
+        cursor = conn.cursor()
+
+        # Insert passed data
+        cursor.execute('INSERT INTO Books (bookId, name, authorId, genre)  VALUES (?, ?, ?, ?);',
+                       (self.bookId, self.name, self.authorId, self.genre))
+
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+
+# Reset Database to default starting data
 # @app.route('/', methods=['POST', 'GET'])
 def reset_database():
     # Change to stored procedure?
-    conn = sqlite3.connect('databases/test_db.db')
+    conn = sqlite3.connect('databases/test_db1.db')
     cursor = conn.cursor()
 
+    # drop tables to reset
     cursor.execute('DROP TABLE IF EXISTS Books;')
     cursor.execute('DROP TABLE IF EXISTS Authors;')
 
-    # Create Authors(authorId, firstname, lastname, birth_year, death_year)
+    # Create tables Authors(authorId, firstname, lastname)
     cursor.execute('CREATE TABLE IF NOT EXISTS Authors '
                    '(authorId INTEGER PRIMARY KEY, '
                    'firstname TEXT, '
                    'lastname TEXT);')
 
-    cursor.execute('INSERT INTO Authors (authorId, firstname, lastname) VALUES (?, ?, ?);', (1, "Leigh", "Bardugo"))
-    cursor.execute('INSERT INTO Authors (authorId, firstname, lastname) VALUES (?, ?, ?);', (2, "Neil", "Gaiman"))
-    cursor.execute('INSERT INTO Authors (authorId, firstname, lastname) VALUES (?, ?, ?);', (3, "William", "Shakespeare"))
-    cursor.execute('INSERT INTO Authors (authorId, firstname, lastname) VALUES (?, ?, ?);', (4, "Agatha", "Christie"))
-    cursor.execute('INSERT INTO Authors (authorId, firstname, lastname) VALUES (?, ?, ?);', (5, "Madeline", "Miller"))
-    cursor.execute('INSERT INTO Authors (authorId, firstname, lastname) VALUES (?, ?, ?);', (6, "Robert", "Frost"))
-    cursor.execute('INSERT INTO Authors (authorId, firstname, lastname) VALUES (?, ?, ?);', (7, "Stephen", "King"))
-    cursor.execute('INSERT INTO Authors (authorId, firstname, lastname) VALUES (?, ?, ?);', (8, "Michelle", "Obama"))
-    cursor.execute('INSERT INTO Authors (authorId, firstname, lastname) VALUES (?, ?, ?);', (9, "Kevin", "Kwan"))
-    cursor.execute('INSERT INTO Authors (authorId, firstname, lastname) VALUES (?, ?, ?);', (10, "Robin", "Kimmerer"))
+    # use ORM to insert each author instance
+
+    newAuthor1 = AuthorClass(1, "Leigh", "Bardugo")
+    newAuthor1.save_to_db()
+
+    newAuthor2 = AuthorClass(2, "Neil", "Gaiman")
+    newAuthor2.save_to_db()
+
+    newAuthor3 = AuthorClass(3, "William", "Shakespeare")
+    newAuthor3.save_to_db()
+
+    newAuthor4 = AuthorClass(4, "Agatha", "Christie")
+    newAuthor4.save_to_db()
+
+    newAuthor5 = AuthorClass(5, "Madeline", "Miller")
+    newAuthor5.save_to_db()
+
+    newAuthor6 = AuthorClass(6, "Robert", "Frost")
+    newAuthor6.save_to_db()
+
+    newAuthor7 = AuthorClass(7, "Stephen", "King")
+    newAuthor7.save_to_db()
+
+    newAuthor8 = AuthorClass(8, "Michelle", "Obama")
+    newAuthor8.save_to_db()
+
+    newAuthor9 = AuthorClass(9, "Kevin", "Kwan")
+    newAuthor9.save_to_db()
+
+    newAuthor10 = AuthorClass(10, "Robin", "Kimmerer")
+    newAuthor10.save_to_db()
 
     # Create Books(bookId, name, authorId, genre, length_in_pages)
     cursor.execute('CREATE TABLE IF NOT EXISTS Books '
@@ -44,30 +110,41 @@ def reset_database():
                    'genre TEXT, '
                    'FOREIGN KEY(authorId) REFERENCES Authors(authorId));')
 
-    cursor.execute('INSERT INTO Books (bookId, name, authorId, genre)  VALUES (?, ?, ?, ?);', (1, "Six of Crows", 1, "Fantasy"))
-    cursor.execute('INSERT INTO Books (bookId, name, authorId, genre)  VALUES (?, ?, ?, ?);', (2, "Coraline", 2, "Fantasy"))
-    cursor.execute('INSERT INTO Books (bookId, name, authorId, genre)  VALUES (?, ?, ?, ?);',
-                   (3, "Romeo and Juliet", 3, "Play"))
-    cursor.execute('INSERT INTO Books (bookId, name, authorId, genre)  VALUES (?, ?, ?, ?);',
-                   (4, "Murder on the Orient Express", 4, "Mystery"))
-    cursor.execute('INSERT INTO Books (bookId, name, authorId, genre)  VALUES (?, ?, ?, ?);',
-                   (5, "The Song of Achilles", 5, "Historical Fiction"))
-    cursor.execute('INSERT INTO Books (bookId, name, authorId, genre)  VALUES (?, ?, ?, ?);',
-                   (6, "The Road Not Taken", 6, "Poetry"))
-    cursor.execute('INSERT INTO Books (bookId, name, authorId, genre)  VALUES (?, ?, ?, ?);',
-                   (7, "It", 7, "Horror"))
-    cursor.execute('INSERT INTO Books (bookId, name, authorId, genre)  VALUES (?, ?, ?, ?);',
-                   (8, "Becoming", 8, "Autobiography"))
-    cursor.execute('INSERT INTO Books (bookId, name, authorId, genre)  VALUES (?, ?, ?, ?);',
-                   (9, "Crazy Rich Asians", 9, "Romance"))
-    cursor.execute('INSERT INTO Books (bookId, name, authorId, genre)  VALUES (?, ?, ?, ?);',
-                   (10, "Braiding Sweetgrass", 10, "Contemporary"))
-    conn.commit()
+    # use ORM to insert each author instance
 
-    # Create stored procedures
-    # with open('stored_procedures.sql', 'r') as sql_file:
-    #    cursor.executescript(sql_file.read())
+    newBook1 = BookClass(1, "Six of Crows", 1, "Fantasy")
+    newBook1.save_to_db()
+
+    newBook1 = BookClass(2, "Coraline", 2, "Fantasy")
+    newBook1.save_to_db()
+
+    newBook1 = BookClass(3, "Romeo and Juliet", 3, "Play")
+    newBook1.save_to_db()
+
+    newBook1 = BookClass(4, "Murder on the Orient Express", 4, "Mystery")
+    newBook1.save_to_db()
+
+    newBook1 = BookClass(5, "The Song of Achilles", 5, "Historical Fiction")
+    newBook1.save_to_db()
+
+    newBook1 = BookClass(6, "The Road Not Taken", 6, "Poetry")
+    newBook1.save_to_db()
+
+    newBook1 = BookClass(7, "It", 7, "Horror")
+    newBook1.save_to_db()
+
+    newBook1 = BookClass(8, "Becoming", 8, "Autobiography")
+    newBook1.save_to_db()
+
+    newBook1 = BookClass(9, "Crazy Rich Asians", 9, "Romance")
+    newBook1.save_to_db()
+
+    newBook1 = BookClass(10, "Braiding Sweetgrass", 10, "Contemporary")
+    newBook1.save_to_db()
+
+
     conn.commit()
+    cursor.close()
     conn.close()
 
     return
@@ -84,26 +161,22 @@ def update_database():
 
 
     # Connect to the database
-    conn = sqlite3.connect('databases/test_db.db')
+    conn = sqlite3.connect('databases/test_db1.db')
     cursor = conn.cursor()
 
     # Add to database using the user's input
 
 
     # Calculate author's id
-
     aId = 0
-
-    # Stored Procedure
-    # cursor.execute('CALL getAuthorIdFromName(?, ?)', (bookauthorfirst, bookauthorsecond))
-
-    # SQLite version ---
     cursor.execute('SELECT authorId FROM Authors a WHERE LOWER(a.firstname) = LOWER(?) AND LOWER(a.lastname) = LOWER(?)',
                    (bookauthorfirst.capitalize(), bookauthorsecond.capitalize()))
     result = cursor.fetchone()
-    aId = result[0]
 
-    # end of procedure ---
+    if (result is not None):
+        aId = result[0]
+    else:
+        aId = None
 
     if (aId is not None):
         # Author already exists, check that they don't already have a book of this name
@@ -121,48 +194,41 @@ def update_database():
         # New author, need to add to the Authors table
 
         # Calculate a new author ID
-
-        # Stored Procedure
-        # cursor.execute('CALL getNewAuthorId()')
-
-        # SQLite version ---
         cursor.execute('SELECT max(authorId) from Authors;')
         result = cursor.fetchone()
         if (result[0] is None):
             aId = 1
         else:
             aId = result[0] + 1
-        # end of procedure ---
+
 
         # Add new author to Authors table
-        cursor.execute('INSERT INTO Authors (authorId, firstname, lastname) VALUES (?, ?, ?);',
-                       (aId, bookauthorfirst.capitalize(), bookauthorsecond.capitalize()))
+        newauthor = AuthorClass(aId, bookauthorfirst.capitalize(), bookauthorsecond.capitalize())
+        newauthor.save_to_db()
+
+        #cursor.execute('INSERT INTO Authors (authorId, firstname, lastname) VALUES (?, ?, ?);', (aId, bookauthorfirst.capitalize(), bookauthorsecond.capitalize()))
 
 
     # Author has been added to database if not previously existing. Book can now be registered
 
     # Calculate a new book ID
     max_id = 0
-
-    # Stored Procedure
-    # cursor.execute('CALL getNewBookId()')
-
-    # SQLite version ---
     cursor.execute('SELECT max(bookId) from Books;')
     result = cursor.fetchone()
 
-    if (result[0] is None):
+    if (result is None or result[0] is None):
         # empty booklist, this will be the first
         max_id = 1
     else:
         # increment largest ID by 1, will be unique ID
         max_id = result[0] + 1
-    # SQLite version ---
 
 
     # Insert -- attribute order is : id, name, genre, author, length
-    cursor.execute('INSERT INTO Books (bookId, name, authorId, genre)  VALUES (?, ?, ?, ?);',
-                   (max_id, bookname, aId, bookgenre.capitalize()))
+    newbook = BookClass(max_id, bookname, aId, bookgenre.capitalize())
+    newbook.save_to_db()
+
+    #cursor.execute('INSERT INTO Books (bookId, name, authorId, genre)  VALUES (?, ?, ?, ?);', (max_id, bookname, aId, bookgenre.capitalize()))
     conn.commit()
     conn.close()
 
@@ -176,7 +242,7 @@ def query_database():
     rating_input = request.form['rating_input']
 
     # Connect to the database
-    conn = sqlite3.connect('databases/test_db.db')
+    conn = sqlite3.connect('databases/test_db1.db')
     cursor = conn.cursor()
 
     # Execute an SQL query using the user's input
@@ -222,5 +288,5 @@ def query_database():
     return render_template('results.html', data=result)
 
 if __name__ == '__main__':
-    # reset_database()
+    reset_database()
     app.run(debug=True)
